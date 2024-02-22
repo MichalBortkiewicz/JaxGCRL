50a869253615afcd0890ba9695640b5b78a5dae2
MichalBortkiewicz <michalbortkiewicz8@gmail.com>
2024-02-22 13:27:26 +0100

    feat: add PPO from brax to local files (is working)

--- humanoid_mj3.py
+++ humanoid_mj3.py
@@ -1,26 +1,15 @@
-from datetime import datetime
 import functools
+from datetime import datetime
+
 import jax
-from jax import numpy as jp
-import numpy as np
-from typing import Any, Dict, Sequence, Tuple, Union
-
-from brax import base
-from brax import envs
-from brax import math
-from brax.base import Base, Motion, Transform
-from brax.envs.base import Env, PipelineEnv, State
-from brax.mjx.base import State as MjxState
-from brax.training.agents.ppo import train as ppo
-from brax.training.agents.ppo import networks as ppo_networks
-from brax.io import html, mjcf, model
-
-from etils import epath
-from flax import struct
-from matplotlib import pyplot as plt
-import mediapy as media
-from ml_collections import config_dict
 import mujoco
+from brax import envs
+from brax.envs.base import PipelineEnv, State
+from brax.io import mjcf
+from crl_new.train import train
+from etils import epath
+from jax import numpy as jp
+from matplotlib import pyplot as plt
 from mujoco import mjx
 
 
@@ -141,62 +130,62 @@ class Humanoid(PipelineEnv):
             ]
         )
 
+if __name__ == "__main__":
+    envs.register_environment("humanoid", Humanoid)
 
-envs.register_environment("humanoid", Humanoid)
-
-#%%
-# instantiate the environment
-env_name = "humanoid"
-env = envs.get_environment(env_name)
-
-
-#%%
-train_fn = functools.partial(
-    ppo.train,
-    num_timesteps=30_000_000,
-    num_evals=5,
-    reward_scaling=0.1,
-    episode_length=1000,
-    normalize_observations=True,
-    action_repeat=1,
-    unroll_length=10,
-    num_minibatches=32,
-    num_updates_per_batch=8,
-    discounting=0.97,
-    learning_rate=3e-4,
-    entropy_cost=1e-3,
-    num_envs=2048,
-    batch_size=1024,
-    seed=0,
-)
+    #%%
+    # instantiate the environment
+    env_name = "humanoid"
+    env = envs.get_environment(env_name)
 
 
-x_data = []
-y_data = []
-ydataerr = []
-times = [datetime.now()]
-
-max_y, min_y = 13000, 0
-
-
-def progress(num_steps, metrics):
-    times.append(datetime.now())
-    x_data.append(num_steps)
-    y_data.append(metrics["eval/episode_reward"])
-    ydataerr.append(metrics["eval/episode_reward_std"])
-    print(f"step: {num_steps}, reward: {y_data[-1]:.3f} +/- {ydataerr[-1]:.3f}")
-    plt.xlim([0, train_fn.keywords["num_timesteps"] * 1.25])
-    plt.ylim([min_y, max_y])
-
-    plt.xlabel("# environment steps")
-    plt.ylabel("reward per episode")
-    plt.title(f"y={y_data[-1]:.3f}")
-
-    plt.errorbar(x_data, y_data, yerr=ydataerr)
-    plt.show()
+    #%%
+    train_fn = functools.partial(
+        train,
+        num_timesteps=30_000_000,
+        num_evals=5,
+        reward_scaling=0.1,
+        episode_length=1000,
+        normalize_observations=True,
+        action_repeat=1,
+        unroll_length=10,
+        num_minibatches=32,
+        num_updates_per_batch=8,
+        discounting=0.97,
+        learning_rate=3e-4,
+        entropy_cost=1e-3,
+        num_envs=2048,
+        batch_size=1024,
+        seed=0,
+    )
 
 
-make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
+    x_data = []
+    y_data = []
+    ydataerr = []
+    times = [datetime.now()]
 
-print(f"time to jit: {times[1] - times[0]}")
-print(f"time to train: {times[-1] - times[1]}")
+    max_y, min_y = 13000, 0
+
+
+    def progress(num_steps, metrics):
+        times.append(datetime.now())
+        x_data.append(num_steps)
+        y_data.append(metrics["eval/episode_reward"])
+        ydataerr.append(metrics["eval/episode_reward_std"])
+        print(f"step: {num_steps}, reward: {y_data[-1]:.3f} +/- {ydataerr[-1]:.3f}")
+        plt.xlim([0, train_fn.keywords["num_timesteps"] * 1.25])
+        plt.ylim([min_y, max_y])
+
+        plt.xlabel("# environment steps")
+        plt.ylabel("reward per episode")
+        plt.title(f"y={y_data[-1]:.3f}")
+
+        plt.errorbar(x_data, y_data, yerr=ydataerr)
+        plt.show()
+
+
+    make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
+
+    print(f"time to jit: {times[1] - times[0]}")
+    print(f"time to train: {times[-1] - times[1]}")
