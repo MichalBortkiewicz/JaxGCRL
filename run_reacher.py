58bfdf139221f44feaf226d8660891b5b4d3fa7b
MichalBortkiewicz <michalbortkiewicz8@gmail.com>
2024-03-03 17:35:17 +0100

    feat: (WIP) add crl critic loss (currently only 0) and print it in vis function
    no negative and positive distinction yet, just first prototype of contrastive loss

--- run_reacher.py
+++ run_reacher.py
@@ -24,13 +24,14 @@ if __name__ == "__main__":
         learning_rate=3e-4,
         # For debug purposes
         num_envs=16,
-        batch_size=64,
+        batch_size=32,
         seed=0,
     )
 
 
     x_data = []
     y_data = []
+    y_data2 = []
     ydataerr = []
     times = [datetime.now()]
 
@@ -38,20 +39,35 @@ if __name__ == "__main__":
 
 
     def progress(num_steps, metrics):
+        print(metrics.keys())
         times.append(datetime.now())
         x_data.append(num_steps)
         y_data.append(metrics["eval/episode_reward"])
         ydataerr.append(metrics["eval/episode_reward_std"])
+
+        if "eval/crl_critic_loss" in metrics.keys():
+            y_data2.append(metrics["eval/crl_critic_loss"])
+        else:
+            y_data2.append(0)
+        fig, axs = plt.subplots(2, 1, figsize=(10, 5))
+
         print(f"step: {num_steps}, reward: {y_data[-1]:.3f} +/- {ydataerr[-1]:.3f}")
-        plt.xlim([0, train_fn.keywords["num_timesteps"] * 1.1])
-        plt.ylim([min_y, max_y])
+        axs[0].set_xlim(0, train_fn.keywords["num_timesteps"] * 1.1)
+        axs[0].set_ylim(min_y, max_y)
+        axs[0].set_xlabel("# environment steps")
+        axs[0].set_ylabel("reward per episode")
+        axs[0].errorbar(x_data, y_data, yerr=ydataerr)
+        axs[0].set_title(f"y={y_data[-1]:.3f}")
 
-        plt.xlabel("# environment steps")
-        plt.ylabel("reward per episode")
-        plt.title(f"y={y_data[-1]:.3f}")
+        axs[1].set_xlim(0, train_fn.keywords["num_timesteps"] * 1.1)
+        axs[1].set_xlabel("# environment steps")
+        axs[1].set_ylabel("crl_critic_loss")
+        axs[1].plot(x_data, y_data2)
+        axs[1].set_title(f"CRL:y={y_data2[-1]:.3f}")
 
-        plt.errorbar(x_data, y_data, yerr=ydataerr)
+        plt.tight_layout()
         plt.show()
+        print(f"CRL loss: {y_data2[-1]}")
 
 
     make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
