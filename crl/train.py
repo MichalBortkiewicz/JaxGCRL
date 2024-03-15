a75920695aaad33356a668b48ab61fa2caf5fe0d
MichalBortkiewicz <michalbortkiewicz8@gmail.com>
2024-03-15 20:07:08 +0100

    fix: goal_index key in sampling (previously constant)

--- crl/train.py
+++ crl/train.py
@@ -60,12 +60,13 @@ class TrajectoryUniformSamplingQueue(QueueBase[Sample], Generic[Sample]):
         batch = jnp.take(buffer_state.data, idx, axis=0, mode="wrap")
 
         transitions = self._unflatten_fn(batch)
-        transitions = jax.vmap(TrajectoryUniformSamplingQueue.flatten_fn)(transitions)
+        batch_keys = jax.random.split(sample_key, self._sample_batch_size)
+        transitions = jax.vmap(TrajectoryUniformSamplingQueue.flatten_fn, in_axes=(0, 0))(transitions, batch_keys)
         return buffer_state.replace(key=key), transitions
 
     @staticmethod
     @jax.jit
-    def flatten_fn(transition: Transition) -> Transition:
+    def flatten_fn(transition: Transition, sample_key:PRNGKey) -> Transition:
         # TODO: we can take more transitions, but they will be from the same trajectory
         # TODO: move outside
         Config = namedtuple("Config", "discount obs_dim start_index end_index")
@@ -85,7 +86,7 @@ class TrajectoryUniformSamplingQueue(QueueBase[Sample], Generic[Sample]):
 
         # TODO: probably use start_index and end_index if needed
         if CRL_TRAINING:
-            goal_index = random.categorical(random.PRNGKey(0), jnp.log(probs))
+            goal_index = random.categorical(sample_key, jnp.log(probs))
             goal = transition.observation[:, : config.obs_dim]
             goal = jnp.take(goal, goal_index[:-1], axis=0)
         else:
