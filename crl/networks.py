3f441c16bceca1c5aec4c628ca90bafd546aa56e
MichalBortkiewicz <michalbortkiewicz8@gmail.com>
2024-03-16 19:17:44 +0100

    chor: make exactly the same policy as in old CRL

--- crl/networks.py
+++ crl/networks.py
@@ -1,10 +1,12 @@
 from typing import Callable, Sequence, Tuple
 
 import flax
+import jax
 import jax.numpy as jnp
 from brax.training import distribution
 from brax.training import networks
 from brax.training import types
+from brax.training.networks import ActivationFn, FeedForwardNetwork, MLP
 from brax.training.types import PRNGKey
 from flax import linen
 
@@ -29,6 +31,30 @@ def make_embedder(
         init=lambda rng: module.init(rng, dummy_obs), apply=apply
     )
     return model
+
+def make_crl_policy_network(
+    param_size: int,
+    obs_size: int,
+    preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+    hidden_layer_sizes: Sequence[int] = (256, 256),
+    activation: ActivationFn = linen.relu,
+) -> FeedForwardNetwork:
+    """Creates a policy network."""
+    policy_module = MLP(
+        layer_sizes=list(hidden_layer_sizes) + [param_size],
+        activation=activation,
+        kernel_init=jax.nn.initializers.lecun_uniform(),
+        activate_final=True,
+    )
+
+    def apply(processor_params, policy_params, obs):
+        obs = preprocess_observations_fn(obs, processor_params)
+        return policy_module.apply(policy_params, obs)
+
+    dummy_obs = jnp.zeros((1, obs_size))
+    return FeedForwardNetwork(
+        init=lambda key: policy_module.init(key, dummy_obs), apply=apply
+    )
 
 @flax.struct.dataclass
 class SACNetworks:
@@ -107,7 +133,7 @@ def make_sac_networks(
     crl_parametric_action_distribution = distribution.NormalTanhDistribution(
         event_size=action_size
     )
-    crl_policy_network = networks.make_policy_network(
+    crl_policy_network = make_crl_policy_network(
         crl_parametric_action_distribution.param_size,
         observation_size,
         preprocess_observations_fn=preprocess_observations_fn,
