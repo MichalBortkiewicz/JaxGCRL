9337fc4c724eb83e704b1868e46ca44769a994e1
MichalBortkiewicz <michalbortkiewicz8@gmail.com>
2024-03-18 09:27:14 +0100

    feat: add new fresh sac implementation

---
+++ crl_new/losses.py
@@ -0,0 +1,110 @@
+from typing import Any
+
+from brax.training import types
+from crl_new import networks as sac_networks
+from brax.training.types import Params
+from brax.training.types import PRNGKey
+import jax
+import jax.numpy as jnp
+
+Transition = types.Transition
+
+
+def make_losses(
+    sac_network: sac_networks.SACNetworks,
+    reward_scaling: float,
+    discounting: float,
+    action_size: int,
+):
+    """Creates the SAC losses."""
+
+    target_entropy = -0.5 * action_size
+    policy_network = sac_network.policy_network
+    q_network = sac_network.q_network
+    parametric_action_distribution = sac_network.parametric_action_distribution
+
+    def alpha_loss(
+        log_alpha: jnp.ndarray,
+        policy_params: Params,
+        normalizer_params: Any,
+        transitions: Transition,
+        key: PRNGKey,
+    ) -> jnp.ndarray:
+        """Eq 18 from https://arxiv.org/pdf/1812.05905.pdf."""
+        dist_params = policy_network.apply(
+            normalizer_params, policy_params, transitions.observation
+        )
+        action = parametric_action_distribution.sample_no_postprocessing(
+            dist_params, key
+        )
+        log_prob = parametric_action_distribution.log_prob(dist_params, action)
+        alpha = jnp.exp(log_alpha)
+        alpha_loss = alpha * jax.lax.stop_gradient(-log_prob - target_entropy)
+        return jnp.mean(alpha_loss)
+
+    def critic_loss(
+        q_params: Params,
+        policy_params: Params,
+        normalizer_params: Any,
+        target_q_params: Params,
+        alpha: jnp.ndarray,
+        transitions: Transition,
+        key: PRNGKey,
+    ) -> jnp.ndarray:
+        q_old_action = q_network.apply(
+            normalizer_params, q_params, transitions.observation, transitions.action
+        )
+        next_dist_params = policy_network.apply(
+            normalizer_params, policy_params, transitions.next_observation
+        )
+        next_action = parametric_action_distribution.sample_no_postprocessing(
+            next_dist_params, key
+        )
+        next_log_prob = parametric_action_distribution.log_prob(
+            next_dist_params, next_action
+        )
+        next_action = parametric_action_distribution.postprocess(next_action)
+        next_q = q_network.apply(
+            normalizer_params,
+            target_q_params,
+            transitions.next_observation,
+            next_action,
+        )
+        next_v = jnp.min(next_q, axis=-1) - alpha * next_log_prob
+        target_q = jax.lax.stop_gradient(
+            transitions.reward * reward_scaling
+            + transitions.discount * discounting * next_v
+        )
+        q_error = q_old_action - jnp.expand_dims(target_q, -1)
+
+        # Better bootstrapping for truncated episodes.
+        truncation = transitions.extras["state_extras"]["truncation"]
+        q_error *= jnp.expand_dims(1 - truncation, -1)
+
+        q_loss = 0.5 * jnp.mean(jnp.square(q_error))
+        return q_loss
+
+    def actor_loss(
+        policy_params: Params,
+        normalizer_params: Any,
+        q_params: Params,
+        alpha: jnp.ndarray,
+        transitions: Transition,
+        key: PRNGKey,
+    ) -> jnp.ndarray:
+        dist_params = policy_network.apply(
+            normalizer_params, policy_params, transitions.observation
+        )
+        action = parametric_action_distribution.sample_no_postprocessing(
+            dist_params, key
+        )
+        log_prob = parametric_action_distribution.log_prob(dist_params, action)
+        action = parametric_action_distribution.postprocess(action)
+        q_action = q_network.apply(
+            normalizer_params, q_params, transitions.observation, action
+        )
+        min_q = jnp.min(q_action, axis=-1)
+        actor_loss = alpha * log_prob - min_q
+        return jnp.mean(actor_loss)
+
+    return alpha_loss, critic_loss, actor_loss
---
+++ crl_new/networks.py
@@ -0,0 +1,69 @@
+from typing import Sequence, Tuple
+
+from brax.training import distribution
+from brax.training import networks
+from brax.training import types
+from brax.training.types import PRNGKey
+import flax
+from flax import linen
+
+
+@flax.struct.dataclass
+class SACNetworks:
+    policy_network: networks.FeedForwardNetwork
+    q_network: networks.FeedForwardNetwork
+    parametric_action_distribution: distribution.ParametricDistribution
+
+
+def make_inference_fn(sac_networks: SACNetworks):
+    """Creates params and inference function for the SAC agent."""
+
+    def make_policy(
+        params: types.PolicyParams, deterministic: bool = False
+    ) -> types.Policy:
+        def policy(
+            observations: types.Observation, key_sample: PRNGKey
+        ) -> Tuple[types.Action, types.Extra]:
+            logits = sac_networks.policy_network.apply(*params, observations)
+            if deterministic:
+                return sac_networks.parametric_action_distribution.mode(logits), {}
+            return (
+                sac_networks.parametric_action_distribution.sample(logits, key_sample),
+                {},
+            )
+
+        return policy
+
+    return make_policy
+
+
+def make_sac_networks(
+    observation_size: int,
+    action_size: int,
+    preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+    hidden_layer_sizes: Sequence[int] = (256, 256),
+    activation: networks.ActivationFn = linen.relu,
+) -> SACNetworks:
+    """Make SAC networks."""
+    parametric_action_distribution = distribution.NormalTanhDistribution(
+        event_size=action_size
+    )
+    policy_network = networks.make_policy_network(
+        parametric_action_distribution.param_size,
+        observation_size,
+        preprocess_observations_fn=preprocess_observations_fn,
+        hidden_layer_sizes=hidden_layer_sizes,
+        activation=activation,
+    )
+    q_network = networks.make_q_network(
+        observation_size,
+        action_size,
+        preprocess_observations_fn=preprocess_observations_fn,
+        hidden_layer_sizes=hidden_layer_sizes,
+        activation=activation,
+    )
+    return SACNetworks(
+        policy_network=policy_network,
+        q_network=q_network,
+        parametric_action_distribution=parametric_action_distribution,
+    )
