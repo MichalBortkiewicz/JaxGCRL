e8f9864fe2066845025e10bf57d9cb488fc0da80
MichalBortkiewicz <michalbortkiewicz8@gmail.com>
2024-04-03 11:33:10 +0200

    feat: add env to args and possibility to run tested hparams + refactor

--- training.py
+++ training.py
@@ -1,43 +1,17 @@
-import argparse
 import functools
+import json
 import os
-from collections import namedtuple
 
 import jax
 import wandb
-# from brax.envs.reacher import Reacher
 from brax.io import model
-# from brax.training.agents.sac.train import train
-from pyinstrument import Profiler
 from brax.io import html
+from pyinstrument import Profiler
 
 
-# from crl.train import train
 from crl_new.train import train
-from envs.ant import Ant
-from envs.debug_env import Debug
-from envs.reacher import Reacher
-from utils import MetricsRecorder
+from utils import MetricsRecorder, get_env_config, create_env, get_tested_args, create_parser
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Training script arguments')
-    parser.add_argument('--exp_name', type=str, default="test", help='Name of the experiment')
-    parser.add_argument('--num_timesteps', type=int, default=1000000, help='Number of training timesteps')
-    parser.add_argument('--max_replay_size', type=int, default=50000, help='Maximum size of replay buffer')
-    parser.add_argument('--min_replay_size', type=int, default=8192, help='Minimum size of replay buffer')
-    parser.add_argument('--num_evals', type=int, default=50, help='Number of evaluations')
-    parser.add_argument('--reward_scaling', type=float, default=0.1, help='Scaling factor for rewards')
-    parser.add_argument('--episode_length', type=int, default=50, help='Maximum length of each episode')
-    parser.add_argument('--normalize_observations', type=bool, default=True, help='Whether to normalize observations')
-    parser.add_argument('--action_repeat', type=int, default=2, help='Number of times to repeat each action')
-    parser.add_argument('--grad_updates_per_step', type=int, default=2, help='Number of gradient updates per step')
-    parser.add_argument('--discounting', type=float, default=0.997, help='Discounting factor for rewards')
-    parser.add_argument('--learning_rate', type=float, default=6e-4, help='Learning rate for the optimizer')
-    parser.add_argument('--num_envs', type=int, default=2048, help='Number of environments')
-    parser.add_argument('--batch_size', type=int, default=512, help='Batch size for training')
-    parser.add_argument('--seed', type=int, default=0, help='Seed for reproducibility')
-    parser.add_argument('--unroll_length', type=int, default=50, help='Length of the env unroll')
-    return parser.parse_args()
 
 def render(inf_fun_factory, params, env, exp_dir, exp_name):
     inference_fn = inf_fun_factory(params)
@@ -63,44 +37,8 @@ def render(inf_fun_factory, params, env, exp_dir, exp_name):
 
 def main(args):
 
-    env = Reacher(backend="spring")
-    env = Ant(backend="spring")
-
-    DEBUG = isinstance(env, Debug)
-    Config = namedtuple(
-        "Config",
-        "sac debug discount obs_dim start_index end_index goal_start_idx goal_end_idx goal_dim unroll_length episode_length repr_dim",
-    )
-    if DEBUG:
-        CONFIG = Config(
-            sac=False,
-            debug=True,
-            discount=args.discounting,
-            obs_dim=2,
-            start_index=0,
-            end_index=2,
-            goal_start_idx=0,
-            goal_end_idx=2,
-            goal_dim=2,
-            unroll_length=args.unroll_length,
-            episode_length=args.episode_length,
-            repr_dim=64,
-        )
-    else:
-        CONFIG = Config(
-            sac=True,
-            debug=False,
-            discount=args.discounting,
-            obs_dim=10,
-            start_index=0,
-            end_index=10,
-            goal_start_idx=4,
-            goal_end_idx=10,
-            goal_dim=6,
-            unroll_length=args.unroll_length,
-            episode_length=args.episode_length,
-            repr_dim=64,
-        )
+    env = create_env(args.env_name)
+    config = get_env_config(args)
 
     train_fn = functools.partial(
         train,
@@ -119,7 +57,7 @@ def main(args):
         batch_size=args.batch_size,
         seed=args.seed,
         unroll_length=args.unroll_length,
-        config=CONFIG
+        config=config
     )
 
     metrics_recorder = MetricsRecorder(args.num_timesteps)
@@ -159,8 +97,24 @@ def main(args):
     render(make_inference_fn, params, env, "./renders", args.exp_name)
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    wandb.init(project="crl", name=args.exp_name, config=vars(args))
+    parser = create_parser()
+    args = parser.parse_args()
+    if args.use_tested_args:
+        args = get_tested_args(args)
+
+    print("Arguments:")
+    print(
+        json.dumps(
+            vars(args), sort_keys=True, indent=4
+        )
+    )
+
+    wandb.init(
+        project="crl",
+        name=args.exp_name,
+        config=vars(args),
+        mode="online" if args.log_wandb else "disabled",
+    )
 
     with Profiler(interval=0.1) as profiler:
         main(args)
--- utils.py
+++ utils.py
@@ -1,7 +1,109 @@
+import argparse
+from collections import namedtuple
 from datetime import datetime
 
 from matplotlib import pyplot as plt
 import wandb
+
+from envs.ant import Ant
+from envs.debug_env import Debug
+from envs.reacher import Reacher
+
+Config = namedtuple(
+    "Config",
+    "sac debug discount obs_dim goal_start_idx goal_end_idx unroll_length episode_length repr_dim",
+)
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Training script arguments')
+    parser.add_argument('--exp_name', type=str, default="test", help='Name of the experiment')
+    parser.add_argument('--num_timesteps', type=int, default=1000000, help='Number of training timesteps')
+    parser.add_argument('--max_replay_size', type=int, default=50000, help='Maximum size of replay buffer')
+    parser.add_argument('--min_replay_size', type=int, default=8192, help='Minimum size of replay buffer')
+    parser.add_argument('--num_evals', type=int, default=50, help='Number of evaluations')
+    parser.add_argument('--reward_scaling', type=float, default=0.1, help='Scaling factor for rewards')
+    parser.add_argument('--episode_length', type=int, default=50, help='Maximum length of each episode')
+    parser.add_argument('--action_repeat', type=int, default=2, help='Number of times to repeat each action')
+    parser.add_argument('--grad_updates_per_step', type=int, default=2, help='Number of gradient updates per step')
+    parser.add_argument('--discounting', type=float, default=0.997, help='Discounting factor for rewards')
+    parser.add_argument('--learning_rate', type=float, default=6e-4, help='Learning rate for the optimizer')
+    parser.add_argument('--num_envs', type=int, default=2048, help='Number of environments')
+    parser.add_argument('--batch_size', type=int, default=512, help='Batch size for training')
+    parser.add_argument('--seed', type=int, default=0, help='Seed for reproducibility')
+    parser.add_argument('--unroll_length', type=int, default=50, help='Length of the env unroll')
+
+    parser.add_argument('--env_name', type=str, default="reacher", help="Name of the environment to train on")
+    parser.add_argument('--normalize_observations', default=False, action="store_true", help='Whether to normalize observations')
+    parser.add_argument('--use_sac', default=False, action="store_true", help="Whether to use SAC or not")
+    parser.add_argument('--use_tested_args', default=False, action="store_true", help='Whether to use tested arguments')
+    parser.add_argument('--log_wandb', default=False, action="store_true", help='Whether to log to wandb')
+
+    return parser
+
+
+def create_env(env_name: str):
+    if env_name == "reacher":
+        env = Reacher(backend="spring")
+    elif env_name == "ant":
+        env = Ant(backend="spring")
+    elif env_name == "debug":
+        env = Debug(backend="spring")
+    else:
+        raise ValueError(f"Unknown environment: {env_name}")
+    return env
+def get_env_config(args: argparse.Namespace):
+    if args.env_name == "debug":
+        config = Config(
+            sac=False,
+            debug=True,
+            discount=args.discounting,
+            obs_dim=2,
+            goal_start_idx=0,
+            goal_end_idx=2,
+            unroll_length=args.unroll_length,
+            episode_length=args.episode_length,
+            repr_dim=64,
+        )
+    elif args.env_name == "reacher":
+        config = Config(
+            sac=True,
+            debug=False,
+            discount=args.discounting,
+            obs_dim=10,
+            goal_start_idx=4,
+            goal_end_idx=10,
+            unroll_length=args.unroll_length,
+            episode_length=args.episode_length,
+            repr_dim=64,
+        )
+    else:
+        raise ValueError(f"Unknown environment: {args.env_name}")
+    return config
+
+def get_tested_args(args):    # Parse arguments
+    if args.env_name=="reacher":
+        parameters = {
+            'num_evals': 10,
+            'seed': 1,
+            'num_timesteps': 500000,
+            'batch_size': 128,
+            'num_envs': 128,
+            'exp_name': 'crl_proper_500',
+            'episode_length': 1000,
+            'unroll_length': 50,
+            'grad_updates_per_step': 8192,
+            'min_replay_size': 0,
+            "normalize_observations": True,
+            "use_sac": False,
+
+        }
+    else:
+        raise ValueError(f"Unknown environment: {args.env_name}")
+    # Update only changed args
+    for key, value in vars(args).items():
+        if key in parameters:
+            setattr(args, key, parameters[key])
+    return args
 
 class MetricsRecorder:
     def __init__(self, num_timesteps):
