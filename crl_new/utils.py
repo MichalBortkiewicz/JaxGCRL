e14783e310d602124424184a1a29e0a940dbb7d2
Vivek Myers <vivek.myers@gmail.com>
2024-07-30 12:52:14 -0700

    numerical stability of CMD1

--- crl_new/utils.py
+++ crl_new/utils.py
@@ -2,11 +2,13 @@ import jax
 import jax.numpy as jnp
 
 def mrn_distance(x, y):
+    eps = 1e-6
     d = x.shape[-1]
     x_prefix = x[..., :d // 2]
     x_suffix = x[..., d // 2:]
     y_prefix = y[..., :d // 2]
     y_suffix = y[..., d // 2:]
     max_component = jnp.max(jax.nn.relu(x_prefix - y_prefix), axis=-1)
-    l2_component = jnp.linalg.norm(x_suffix - y_suffix, axis=-1)
+    l2_component = jnp.sqrt(jnp.square(x_suffix - y_suffix).sum(axis=-1) + eps)
     return max_component + l2_component
+
