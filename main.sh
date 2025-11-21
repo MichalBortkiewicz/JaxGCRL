d71e7d2c24aeb164ef3f7aa962add55830c34ab3
ishirgarg <gargishir@gmail.com>
2025-11-21 15:03:16 -0800

    Add logging for momentum

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 echo "Starting first job..."
 
-CUDA_VISIBLE_DEVICES=6 python3 run.py crl --env ant_u_maze --total-env-steps 120000000 --goal-proposer-name metric --goal-proposal-prob 0.5 > run1.out 2>&1
+CUDA_VISIBLE_DEVICES=7 python3 run.py crl --env ant_u_maze --total-env-steps 100000000 --goal-proposal-prob 0.5 --goal-proposer-name replay_buffer --use-adaptive-mixing --adaptive-mixing-momentum 0.999 > run1.out 2>&1
 
-CUDA_VISIBLE_DEVICES=6 python3 run.py crl --env ant --total-env-steps 80000000 --goal-proposer-name metric --goal-proposal-prob 0.5 > run1.out 2>&1
+CUDA_VISIBLE_DEVICES=7 python3 run.py crl --env ant_u_maze --total-env-steps 100000000 --goal-proposer-name replay_buffer --goal-proposal-prob 0.25 > run2.out 2>&1
+
 
 echo "All jobs completed."
