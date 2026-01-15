15cb2aee01e24b0028a893580a9c545908b1f645
Ishir Garg <gargishir@gmail.com>
2026-01-15 02:12:39 -0800

    bug fix?

--- jaxgcrl/agents/crl/goals.py
+++ jaxgcrl/agents/crl/goals.py
@@ -269,7 +269,7 @@ class DISCOVERGoalProposal(GoalProposer):
         
         current_alpha = jax.experimental.io_callback(
             get_current_alpha_callback,
-            jnp.float32,  # Return type
+            jax.ShapedArray((), jnp.float32),  # Return type: scalar float32
             jnp.float32(self.alpha_0)  # Default alpha
         )
         
